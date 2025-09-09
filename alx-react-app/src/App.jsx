9c91d8da1da463f792b0251713285d1b3ae25f96
Rosemond Ampomah <amprosemond@gmail.com>
2025-09-09 14:29:08 +0000

    Error fixed

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+// import Header from "./Header";
+// import MainContent from "./MainContent";
+// import Footer from "./Footer";
+// import UserProfile from "./components/UserProfile";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-   return (
+  return (
     <>
-      <Header />
+      {/* <Header />
       <MainContent />
-      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer /> */}
+      <WelcomeMessage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
