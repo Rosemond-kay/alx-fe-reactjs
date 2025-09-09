d49ef9ff5d6aa768f07a0fb75423c478bb88af7a
Rosemond Ampomah <amprosemond@gmail.com>
2025-09-09 14:41:46 +0000

    Header, Main and Footer components added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,19 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
-// import Header from "./Header";
-// import MainContent from "./MainContent";
-// import Footer from "./Footer";
-// import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* <Header />
+      <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer /> */}
+      <Footer />
       <WelcomeMessage />
     </>
   );
