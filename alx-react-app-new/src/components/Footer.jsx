d85df8c2b246e2af65e02eb4d172800ce27d4488
Rosemond Ampomah <amprosemond@gmail.com>
2025-09-14 07:31:06 +0000

    Inline CSS Styling Added

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "greenyellow" }}>
+      <p style={{ color: "red" }}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: "paleturquoise" }}>
+      <p style={{ color: "brown", fontWeight: "bold" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
