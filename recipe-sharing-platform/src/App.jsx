a910c0dee57660072d73467ae24c4992f604de8e
Rosemond Ampomah <amprosemond@gmail.com>
2025-10-01 23:15:06 +0000

    HomePage recipe cards created created

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <div className="text-4xl font-bold text-blue-500">Hello Tailwind!</div>
+    <div className="min-h-screen bg-gray-100">
+      <HomePage />
+    </div>
   );
 }
 
