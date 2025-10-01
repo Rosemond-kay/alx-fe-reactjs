f78aa5b21d2086fd29d543dceb88ce1b13d32e27
Rosemond Ampomah <amprosemond@gmail.com>
2025-10-01 22:29:58 +0000

    React and TailwindCSS installed

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-4xl font-bold text-blue-500">Hello Tailwind!</div>
+  );
+}
+
+export default App;
